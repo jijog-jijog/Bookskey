3cdc479ee7f8bea493136df6671eac40eeb4d1cd
jijog-jijog <jijo.cs22@stellamaryscoe.edu.in>
2025-02-17 21:31:59 +0530

    Add initial HTML, CSS, and JavaScript files for the application

---
+++ script.js
@@ -0,0 +1,8 @@
+var show = document.querySelector(".show");
+var close = document.querySelector(".close");
+var box = document.querySelector(".blackbox .content")
+
+
+function rele() {
+    box.style.display = "block";
+}
